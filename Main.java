642eefe97988ebb25fc1bfbfef33ce15090f3158
ABHINAV JAIN <ajain0485@gmail.com>
2020-12-06 13:49:24 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.print("sum of these numbers is: ");
+		int num1=2;
+		int num2=4;
+		int num3=5;
+		int sum=num1+num2+num3;
+	System.out.println(sum );
+}
+    
+}
+
